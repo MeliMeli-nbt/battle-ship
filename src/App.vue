64ec2ea3cd217c3df810fcb28983807cf4b0fc20
Bá Tuấn <tuannb@stringee.com>
2023-10-28 21:16:07 +0700

    create-base-vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
 	<h1 class="text-3xl font-bold underline">
 		Hello world!
-    </h1>
+  </h1>
 </template>
 
 <style scoped>
